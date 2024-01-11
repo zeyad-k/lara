f1ad1819f22bfac0da0f6da17a938095beb53ecf
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-01-11 13:13:48 +0200

    creating a form blade

---
+++ simple_crud_app/resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Simple Crud App</title>
+</head>
+<body>
+    <div class="container">
+        <div class="registeration-area">
+            <h2>Register</h2>
+            <form action="/register" method="post">
+                <input type="text" name="name" class="name">
+                <input type="email" name="email" class="email">
+                <input type="password" name="password" class="password">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
